673b97b5b9f9042991c8cb96d060e8cf9e4de276
YagizCK <yagizkocabiyik20@gmail.com>
2025-06-25 23:08:44 +0300

    Handling Errors

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,19 +1,16 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
-import { isValidUUID } from "@/app/lib/utils";
 import { notFound } from "next/navigation";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const id = params.id;
-  if (!isValidUUID(id)) {
-    notFound();
-  }
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
   if (!invoice) {
     notFound();
   }
--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -67,9 +67,3 @@ export const generatePagination = (currentPage: number, totalPages: number) => {
     totalPages,
   ];
 };
-
-export const isValidUUID = (str: string) => {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/.test(
-    str
-  );
-};
